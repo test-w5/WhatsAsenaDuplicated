fc2ee87f0864b55032a3cf7ebc0a1fb23e7d6486
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-07-19 18:51:02 +0530

    🤔🤔🤔

---
+++ plugins/semoji.js
@@ -0,0 +1,57 @@
+const Asena = require('../events');
+
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
+const fs = require('fs');
+
+const axios = require('axios');
+
+const request = require('request');
+
+const got = require("got");
+
+const Config = require('../config');
+
+const des = "You Can Png From Any Emoji"
+
+const iii = "Only work with emoji"
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'semoji ?(.*)', fromMe: true, desc: des}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker ,{quoted: message.data}
+              );
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'semoji ?(.*)', fromMe: false, desc: des}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker ,{quoted: message.data}
+              );
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+
+    Asena.addCommand({pattern: 'semoji ?(.*)', fromMe: true,dontAddCMDList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker ,{quoted: message.data}
+              );
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+}
+
+}
