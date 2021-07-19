311beab8940d3e12a16f658a73c3c5e2c32658b8
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-07-19 14:04:02 +0530

    🤔🤔🤔

---
+++ plugins/emoji.js
@@ -0,0 +1,55 @@
+const Asena = require('../events');
+
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
+const fs = require('fs');
+
+const axios = require('axios');
+
+const request = require('request');
+
+const got = require("got");
+
+const Config = require('../config');
+
+const des = "You Can Png From Any Emoji"
+
+const iii = "Only work with emoji"
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'stickemoji ?(.*)', fromMe: true, desc: des}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*🐱W5-BOT🤖*'})
+
+    }));
+
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'stickemoji ?(.*)', fromMe: false, desc: des}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*🐱W5-BOT🤖*'})
+
+    }));
+
+    Asena.addCommand({pattern: 'stickemoji ?(.*)', fromMe: true,dontAddCMDList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*🐱W5-BOT🤖*'})
+
+    }));
+
+}
