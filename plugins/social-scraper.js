3538066b9718af4c64c8bdc9e4b421a58e742f83
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-07-19 11:54:59 +0530

    🤔🤔🤔

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -15,30 +15,41 @@ const Tlang = Language.getString('tiktok')
 
 if (cn.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
+    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, desc: "Download content from insta link"}, async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORDIGTV))
+
+    await message.sendMessage(infoMessage("⏳Loading"))
+
+    await axios
+      .get(`https://videfikri.com/api/igtv/?url=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          type,
+        } = response.data.result[0]
+
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+
+        const msg = `${type}`
+
+	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: msg,
+        })}
+		 	 
+	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: msg,
+        })}
 	
-        let urls = `https://videfikri.com/api/igtv/?url=${match[1]}`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
-
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
-
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
-
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.FULLNAME + '* ' + json.result.full_name + '\n*' + Tlang.CAPTION + '* ' + json.result.caption + '\n*' + Tlang.COMMENT + '* ' + json.result.comment + '\n*' + Tlang.DURATION + '* ' + json.result.duration + '\n*' + Tlang.URL + '* ' + json.result.thumb }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.FULLNAME + '* ' + json.result.fullname + '\n*' + Tlang.HEIGHT&WIDTH + '* ' + json.result.height_width + '\n*' + Tlang.CAPTION + '* ' + json.result.caption + '\n*' + Tlang.COMMENT + '* ' + json.result.comment + '\n*' + Tlang.LIKES + '* ' + json.result.likes + '\n*' + Tlang.IMAGEPREDICTION + '* ' + json.result.image_text });
-            }
-        });
-
-    }));
+        
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage("error.Please check the link")),
+      )
+  },
+)
 Asena.addCommand({ pattern: 'twt ?(.*)', fromMe: false, desc: "download from twitter links" }, async (message, match) => {
 
     const userName = match[1]
@@ -108,30 +119,41 @@ Asena.addCommand({ pattern: 'twt ?(.*)', fromMe: false, desc: "download from twi
 }
 else if (cn.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
+    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: "Download content from insta link"}, async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORDIGTV))
+
+    await message.sendMessage(infoMessage("⏳Loading"))
+
+    await axios
+      .get(`https://videfikri.com/api/igtv/?url=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          type,
+        } = response.data.result[0]
+
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+
+        const msg = `${type}`
+
+	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: msg,
+        })}
+		 	 
+	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: msg,
+        })}
 	
-        let urls = `https://videfikri.com/api/igtv/?url=${match[1]}`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
-
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
-
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
-
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.FULLNAME + '* ' + json.result.full_name + '\n*' + Tlang.CAPTION + '* ' + json.result.caption + '\n*' + Tlang.COMMENT + '* ' + json.result.comment + '\n*' + Tlang.DURATION + '* ' + json.result.duration + '\n*' + Tlang.URL + '* ' + json.result.thumb }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.FULLNAME + '* ' + json.result.fullname + '\n*' + Tlang.HEIGHT&WIDTH + '* ' + json.result.height_width + '\n*' + Tlang.CAPTION + '* ' + json.result.caption + '\n*' + Tlang.COMMENT + '* ' + json.result.comment + '\n*' + Tlang.LIKES + '* ' + json.result.likes + '\n*' + Tlang.IMAGEPREDICTION + '* ' + json.result.image_text });
-            }
-        });
-
-    }));
+        
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage("error.Please check the link")),
+      )
+  },
+)
     Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, desc: Lang.DESC, dontAddCommandList: true }, (async (message, match) => {
         if (match[0].includes('install')) return;
         if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
