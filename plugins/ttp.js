eda692e04d1dfd4eb30af5bc3b9388ae8eabc74f
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-07-19 18:40:19 +0530

    😒😒😒

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -52,10 +52,5 @@ else if (Config.WORKTYPE == 'public') {
         var ttinullimage = await axios.get('https://videfikri.com/api/textmaker/glowingneon/?text=' + uri, { responseType: 'arraybuffer' })
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, quoted: message.data, caption: 'W5-BOT' })
     }));
-    Asena.addCommand({ pattern: 'semoji ?(.*)', fromMe: false, desc: Lang.EMOJI_DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_EMOJI, MessageType.text);
-        var uri = encodeURI(match[1])
-        var ttinullimage = await axios.get('https://api.zeks.xyz/api/emoji-image?apikey=w6pOcZAlefcsPoNoFV8CzWpo9yT&emoji=' + uri, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
-    }));
+   
 }
