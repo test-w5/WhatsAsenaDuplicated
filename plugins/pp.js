1e1fd68b56de935fffd0fd65fbe160ec825b9a97
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-07-22 20:14:03 +0530

    🤔🤔🤔

---
+++ plugins/pp.js
@@ -0,0 +1,49 @@
+/* Codded by @WH173-5P1D3R
+Telegram: t.me/WH173-5P1D3R
+Instagram: https://instagram.com/wh173_5p1d3r_official
+Special Thanks:
+@Phaticusthiccy for Unlimitted Helps
+*/
+
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.AN}, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    r_text[0] = "https://i.ibb.co/GVh2zp1/W5-BOT.webp";
+    r_text[1] = "https://i.ibb.co/GVh2zp1/W5-BOT.webp";  
+      
+    var i = Math.floor(2*Math.random())   
+    
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, quoted: message.data, caption: '*Bot Name:* 🐱W5-BOT🤖             *Version:* v2.0'})
+
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.AN}, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    r_text[0] = "https://i.ibb.co/GVh2zp1/W5-BOT.webp";
+    r_text[1] = "https://i.ibb.co/GVh2zp1/W5-BOT.webp";
+    
+    var i = Math.floor(2*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, quoted: message.data, caption: '*Bot Name:* 🐱W5-BOT🤖             *Version:* v2.0'})
+
+    }));
+}  
