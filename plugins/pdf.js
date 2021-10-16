15dfd6fa20e671bcb5001f342f076ef084093f95
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-10-17 00:26:44 +0530

    Create pdf.js

---
+++ plugins/pdf.js
@@ -0,0 +1,22 @@
+let Asena = require('../events');
+let {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+let fs = require('fs');
+let axios = require('axios');
+let request = require('request');
+let got = require("got");
+let Config = require('../config');
+let Language = require('../language');
+let Lang = Language.getString('unvoice');
+let Asena = Config.WORKTYPE == 'public' ? false : true
+
+   Asena.addCommand({pattern: 'pdf ?(.*)', fromMe: toxicdevil, desc: 'Converts site to PDF.' }, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid, '*You must send a link!*', MessageType.text);
+
+    var webimage = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=Y1ZbfT8mS5CCSqKmw92iAuUhyuqD1ohz9AKtxbLNEY4hC2B4G2lVbNVPH6ljVDro`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid, '```Converting Site to PDF```', MessageType.text);
+
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf, quoted: message.data, filename: 'W5-BOT.pdf'});
+
+    }));    
