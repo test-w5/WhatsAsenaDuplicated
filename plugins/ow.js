36d84a8ef774b49c734a2e0424df0530af0705a3
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-10-29 15:23:18 +0530

    🤷‍♂️

--- plugins/ow.js
+++ plugins/ow.js
@@ -6,12 +6,12 @@ const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.NUMBER}, (async (message, match) => {
-const vcard = 'BEGIN:VCARD\n'
+   const w5 = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
             + 'FN:WH173 5P1D3R\n'
             + 'ORG:W5-BOT Owner WH173 5P1D3R;\n' 
             + 'TEL;type=CELL;type=VOICE;waid=917736807522:+917736807522\n'
             + 'END:VCARD'
-await message.client.sendMessage(message.jid, {displayname: "WH173 5P1D3R", vcard: WH173 5P1D3R}, MessageType.contact);
-
+await message.client.sendMessage(message.jid, {displayname: "WH173 5P1D3R", vcard: w5}, MessageType.contact);
+ 
   }));
