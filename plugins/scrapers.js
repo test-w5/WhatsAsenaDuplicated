0763bf285d91707a1db78cb2fd78a210d2808ee7
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-11-08 00:27:13 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -640,7 +640,7 @@ else if (config.WORKTYPE == 'public') {
                     });
                 writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG,MessageType.text);
+                reply = await message.client.sendMessage(message.jid,MessageType.text);
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {mimetype: Mimetype.mp4Audio, quoted: message.data, filename: 'W5-BOT-Songs.mp3'});
             });
     }));
