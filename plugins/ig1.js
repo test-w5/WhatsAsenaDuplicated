6d9f0b62366825a9291d3fa18eff66f57f08310c
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-12-08 22:59:40 +0530

    Create ig1.js

---
+++ plugins/ig1.js
@@ -0,0 +1,110 @@
+
+
+const Asena = require('../events');
+const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const Alexa = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('W5-BOT');
+const ALang = Language.getString('scrapers');
+let wk = Config.WORKTYPE == 'public' ? false : true
+
+Asena.addCommand({ pattern: 'igp ?(.*)', fromMe: wk, desc: Lang.PINSTA}, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Photo* ")
+
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+ALang.DOWNLOADING_VIDEO)
+	
+			var url = `https://yuzzu-api.herokuapp.com/api/instagram?link=${link}`
+
+				await axios
+					.get(`${url}`)
+					.then(async(response) => {
+						const {link,} = response.data.result.result
+						
+
+						const linkdata = await axios.get(link, {responseType: 'arraybuffer'})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.image, {caption: Alexa.BOT_NAME,})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO PHOTO FOUND*"),
+							)
+					})
+					
+}) 
+
+Asena.addCommand({ pattern: 'igp ?(.*)', fromMe: wk, dontAddCommandList:true}, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Photo* ")
+
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+ALang.DOWNLOADING_VIDEO)
+	
+			var url = `https://yuzzu-api.herokuapp.com/api/instagram?link=${link}`
+
+				await axios
+					.get(`${url}`)
+					.then(async(response) => {
+						const {link,} = response.data.result.result
+						
+
+						const linkdata = await axios.get(link, {responseType: 'arraybuffer'})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.image, {caption: Alexa.BOT_NAME,})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO PHOTO FOUND*"),
+							)
+					})
+					
+}) 
+Asena.addCommand({ pattern: 'igv ?(.*)', fromMe: wk, desc: Lang.VINSTA }, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Video* ")
+
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+ALang.DOWNLOADING_VIDEO)
+
+			var url = `https://yuzzu-api.herokuapp.com/api/instagram?link=${link}`
+
+				await axios
+					.get(`${url}`)
+					.then(async(response) => {
+						const {link,} = response.data.result.result
+
+						const linkdata = await axios.get(link, {responseType: 'arraybuffer'})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.video, {caption: Alexa.BOT_NAME,})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO VIDEO FOUND*"),
+							)
+					})
+					
+}) 
+Asena.addCommand({ pattern: 'igv ?(.*)', fromMe: wk, dontAddCommandList:true}, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Video* ")
+
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+ALang.DOWNLOADING_VIDEO)
+
+			var url = `https://yuzzu-api.herokuapp.com/api/instagram?link=${link}`
+
+				await axios
+					.get(`${url}`)
+					.then(async(response) => {
+						const {link,} = response.data.result.result
+
+						const linkdata = await axios.get(link, {responseType: 'arraybuffer'})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.video, {caption: Alexa.BOT_NAME,})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO VIDEO FOUND*"),
+							)
+					})
+					
+}) 
