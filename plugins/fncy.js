f5dd3ff8dd3f81995389fbceab73216207dc04b8
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2022-01-27 23:09:45 +0530

    Update fncy.js

--- plugins/fncy.js
+++ plugins/fncy.js
@@ -1,4 +1,4 @@
-const Asena = require('../events');
+/*/const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const Config = require('../config');
@@ -55,5 +55,5 @@ await axios
         })
 	})    
 
-})
+})/*/
  
