0241a209c112d31f41f42ce849649c0216f8320d
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2022-01-27 21:06:42 +0530

    Update idl.js

--- plugins/idl.js
+++ plugins/idl.js
@@ -1,17 +1,6 @@
 const Asena = require('../events')
 const { MessageType, Mimetype } = require("@adiwajshing/baileys")
-const {
-  getBuffer,
-  h2k,
-  generateMessageID,
-  getGroupAdmins,
-  getRandom,
-  banner,
-  start,
-  info,
-  success,
-  close,
-} = require("./lib/functions")
+const { getBuffer, getRandom, success, close, start } = require("./lib/functions")
 const { yta, ytv, igdl, upload, formatDate } = require("./lib/dl")
 const Language = require("../language")
 const Lang = Language.getString("insta")
