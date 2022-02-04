eda32ce7f668c797214be3defd70f365259277ea
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2022-02-04 09:05:09 +0530

    Create hp.js

---
+++ hp.js
@@ -0,0 +1,16 @@
+function successfullMessage(msg) {
+    return "🐱 W5-BOT:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🐱 W5-BOT:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "🐱 W5-BOT:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
