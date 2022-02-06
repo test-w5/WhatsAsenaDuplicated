bb48331da2567dd64152b9dd293f651f54856f7f
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2022-02-06 21:35:14 +0530

    Update idl.js

--- plugins/idl.js
+++ plugins/idl.js
@@ -1,9 +1,9 @@
-const Asena = require('../events')
-const { MessageType, Mimetype } = require("@adiwajshing/baileys")
-const { getBuffer, getRandom, success, close, start } = require("../lib/functions")
-const { yta, ytv, igdl, upload, formatDate } = require("../lib/dl")
-const Language = require("../language")
-const Lang = Language.getString("insta")
+const Asena = require('../events');
+const { MessageType, Mimetype } = require("@adiwajshing/baileys");
+const { getBuffer, getRandom, success, close, start } = require('../fun');
+const { yta, ytv, igdl, upload, formatDate } = require('../don');
+const Language = require("../language");
+const Lang = Language.getString("insta");
 Asena.addCommand(
   {
     pattern: "ins ?(.*)",
