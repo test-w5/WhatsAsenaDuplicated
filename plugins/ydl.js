4cc96151f30a96439347ced13344610858c55bc0
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2022-02-09 21:47:15 +0530

    Update ydl.js

--- plugins/ydl.js
+++ plugins/ydl.js
@@ -3,9 +3,11 @@ const Language = require("../language");
 const Lang = Language.getString("weather");
 const yts = require("yt-search");
 const moment = require("moment");
-const { yta, ytv, igdl, upload, formatDate } = require('../don');
-const { getBuffer, getRandom, success, close, start } = require('../fun');
-const { getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('../fun');
+//const { yta, ytv, igdl, upload, formatDate } = require('../don');
+//const { getBuffer, getRandom, success, close, start } = require('../fun');
+//const { getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('../fun');
+const { getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, start, info, success, banner, close } = require('./sql/buff');
+const { yta, ytv, igdl, upload, formatDate } = require('./sql/down');
 const { Mimetype, MessageType } = require("@adiwajshing/baileys");
 
 const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
