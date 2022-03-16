9d3e327d91017354c652b6af5984460d145abd18
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2022-03-16 17:34:51 +0530

    Create pindl.js

---
+++ plugins/pindl.js
@@ -0,0 +1,28 @@
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const Asena = require('../events');
+const Config = require('../config');
+const { pinterest } = require('../pin.js')
+const { errorMessage, infoMessage } = require('../hp');
+const axios = require('axios');
+const fs = require('fs');
+const gis = require('g-i-s');
+const got = require("got");
+
+let wk = Config.WORKTYPE == 'public' ? false : true
+
+
+Asena.addCommand({pattern: 'ig ?(.*)', fromMe: wk, desc: "Download from Instagram"}, async (message, match) => {
+  try{
+    if (!match[1]) return await message.sendMessage("Enter a link");
+    await message.sendMessage("Searching...")
+
+    res = await pinterest(match[1])
+    ytm = res.result
+    const profileBuffer = await axios.get(`${ytm.link}`, {responseType: 'arraybuffer'})
+    const msg = `${ytm.link}`
+     if (msg.includes('.mp4')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: "_*🐱W5-BOT🤖*_", quoted: message.data })}
+     if (msg.includes('.jpg')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: "_*🐱W5-BOT🤖*_", quoted: message.data })}
+  } catch {
+     await message.sendMessage("error")
+  }
+});
