0e17fa66d8e19249bb54c069b5c7aeac0e7fb04a
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2022-03-23 22:33:10 +0530

    Update test-story.js

--- plugins/test-story.js
+++ plugins/test-story.js
@@ -1,29 +1,28 @@
-/*const Asena = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const fs = require('fs');
-const got = require("got");
-const axios = require('axios');
-const setting = require('../config');
-const raganork = require('raganork-bot');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const Asena = require('../events');
 const Config = require('../config');
-Asena.addCommand({ pattern: 'story ?(.*)', fromMe: false, desc:'Downloads full/single story from instagram',usage:'.story username or link'}, (async (msg, query) => {
-if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
-var user = query[1];
-var res = await raganork.query.getStory(user,v)
-if (res === "false") return await msg.client.sendMessage(msg.jid, "_Story not found!_", MessageType.text, {quoted: msg.data})
-if (res.error) return await msg.client.sendMessage(msg.jid, res.error.replace('status','story'), MessageType.text, {quoted: msg.data})
-var url = ''
-await msg.sendMessage('```Downloading '+res.result.stories.length+' stories of '+res.result.username+'```');
-res.result.stories.map((result) => {
-url += result.url + ','});
-var que = url !== false ? url.split(',') : [];
-for (var i = 0; i < (que.length < res.result.stories.length ? que.length : res.result.stories.length); i++) {
-var get = got(que[i], {https: {rejectUnauthorized: false}});
-var type = que[i].includes('mp4') ? MessageType.video : MessageType.image
-var mime = que[i].includes('mp4') ? Mimetype.mp4 : Mimetype.jpg
-var stream = get.buffer();
-stream.then(async (video) => {
-await msg.client.sendMessage(msg.jid, video, type, { mimetype: mime,quoted: msg.data});
-})};
-}));
-*/
+const { igstory } = require('../igst.js')
+const { errorMessage, infoMessage } = require('../hp');
+const axios = require('axios');
+const fs = require('fs');
+const gis = require('g-i-s');
+const got = require("got");
+
+let wk = Config.WORKTYPE == 'public' ? false : true
+
+
+Asena.addCommand({pattern: 'story ?(.*)', fromMe: wk, desc: "Download from Instagram"}, async (message, match) => {
+  try{
+    if (!query[1]) return await message.sendMessage("Enter a link");
+    await message.sendMessage("Searching...")
+
+    res = await igstory(query[1])
+    ytm = res.result
+    const profileBuffer = await axios.get(`${ytm.username}`, {responseType: 'arraybuffer'})
+    const msg = `${ytm.username}`
+     if (msg.includes('.mp4')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: "_*🐱W5-BOT🤖*_", quoted: message.data })}
+     if (msg.includes('.jpg')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: "_*🐱W5-BOT🤖*_", quoted: message.data })}
+  } catch {
+     await message.sendMessage("error")
+  }
+});
