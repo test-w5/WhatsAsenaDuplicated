accb9a06111f6b8d7dec8c95d21b8dade49411dd
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2022-03-27 14:17:16 +0530

    Update emoji.js

--- plugins/emoji.js
+++ plugins/emoji.js
@@ -1,21 +1,15 @@
 const Asena = require('../events');
-
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-
 const fs = require('fs');
-
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const cwebp = require('cwebp-bin');
 const axios = require('axios');
-
 const request = require('request');
-
 const got = require("got");
-
 const Config = require('../config');
-
 const des = "Emoji to Emoji Png Picture"
-
 const iii = "Only work with emoji"
-
 if (Config.WORKTYPE == 'private') {
 
     Asena.addCommand({pattern: 'pemoji ?(.*)', fromMe: true, desc: des}, (async (message, match) => {
@@ -39,6 +33,16 @@ else if (Config.WORKTYPE == 'public') {
         var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*🐱W5-BOT🤖*'})
+
+    }));
+    
+    Asena.addCommand({pattern: 'emomix ?(.*)', fromMe: false, desc: des}, (async (message, match) => {
+
+        if(!match) return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://early-pie-production.up.railway.app/emomix?q=${encodeURIComponent(match)}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.sticker, {quoted: message.data})
 
     }));
 
